07041bcebf99ce1db8652e98aff3e0ef7816d262
srikanthsree50 <srikanthsree50@gmail.com>
2021-04-22 18:08:24 +0530

    Angular app with nodejs and express

--- MeanApp/src/app/app.module.ts
+++ MeanApp/src/app/app.module.ts
@@ -1,15 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import { PostCreateComponent} from './posts/post-create/post-create.component';
-import { __importDefault } from 'tslib';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule,MatCardModule,MatButtonModule} from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
+} from "@angular/material";
+
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -17,9 +29,12 @@ import { MatInputModule,MatCardModule,MatButtonModule} from '@angular/material';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
